7c4bd302669726b27a60f624532a710735eec160
SalehKhalilMP <saleh.khalil@mercadolivre.com>
2023-12-14 09:27:42 -0300

    feat: create and implements email as value object

--- internal/core/domain/customer.go
+++ internal/core/domain/customer.go
@@ -2,10 +2,11 @@ package domain
 
 import (
 	. "github.com/hcsouza/fiap-tech-fast-food/internal/core/valueObject/cpf"
+	. "github.com/hcsouza/fiap-tech-fast-food/internal/core/valueObject/email"
 )
 
 type Customer struct {
 	Name  string `json:"name"`
-	Email string `json:"email"`
+	Email Email  `json:"email"`
 	CPF   CPF    `json:"cpf"`
 }
