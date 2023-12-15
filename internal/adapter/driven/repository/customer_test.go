148793fbcbe1a0e48b007c35d7ccb348580d5ea5
SalehKhalilMP <saleh.khalil@mercadolivre.com>
2023-12-15 10:59:23 -0300

    test: implements MockDatabaseAdapter

--- internal/adapter/driven/repository/customer_test.go
+++ internal/adapter/driven/repository/customer_test.go
@@ -6,13 +6,18 @@ import (
 	"github.com/hcsouza/fiap-tech-fast-food/internal/adapter/driven/repository"
 	"github.com/hcsouza/fiap-tech-fast-food/internal/core/domain"
 	. "github.com/hcsouza/fiap-tech-fast-food/internal/core/valueObject/cpf"
+	"github.com/hcsouza/fiap-tech-fast-food/test/mocks"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestCustomerRepository(t *testing.T) {
+	databaseAdapter := &mocks.MockDatabaseAdapter{}
 	t.Run("Should find a customer by CPF", func(t *testing.T) {
-		customerRepository := repository.NewCustomerRespository(nil)
 		cpf := CPF("12345678900")
+
+		databaseAdapter.On("FindOne", string(cpf)).Return(&domain.Customer{}, nil)
+
+		customerRepository := repository.NewCustomerRespository(databaseAdapter)
 
 		customer, err := customerRepository.Find(cpf)
 
@@ -20,12 +25,22 @@ func TestCustomerRepository(t *testing.T) {
 		assert.NotNil(t, customer)
 	})
 	t.Run("Should save a customer", func(t *testing.T) {
-		customerRepository := repository.NewCustomerRespository(nil)
+		cpf := CPF("12345678900")
 		customer := &domain.Customer{
 			Name:  "John Doe",
 			CPF:   CPF("12345678900"),
 			Email: "john.doe@email.com",
 		}
+
+		databaseAdapter.
+			On(
+				"Save",
+				string(cpf),
+				map[string]interface{}{"name": customer.Name, "email": customer.Email},
+			).
+			Return(nil)
+
+		customerRepository := repository.NewCustomerRespository(databaseAdapter)
 
 		err := customerRepository.Save(customer)
 
