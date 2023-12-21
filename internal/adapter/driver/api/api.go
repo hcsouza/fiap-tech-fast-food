607e0dacc2370d8146b66d7a61459799d7f0d02d
Hugo Souza <hugo.souza@ifood.com.br>
2023-12-21 08:15:14 -0300

    feat: change api port  from config

--- internal/adapter/driver/api/api.go
+++ internal/adapter/driver/api/api.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/hcsouza/fiap-tech-fast-food/internal/adapter/driver/api/v1/routes"
+	"github.com/hcsouza/fiap-tech-fast-food/internal/adapter/infra/config"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -19,5 +20,5 @@ func Run(gServer *gin.Engine, dbClient mongo.Client) {
 	api := gServer.Group("/api")
 	routes.RegisterBusinessRoutes(api, dbClient)
 
-	gServer.Run(fmt.Sprintf(":%s", "8080"))
+	gServer.Run(fmt.Sprintf(":%s", config.GetApiCfg().Port))
 }
