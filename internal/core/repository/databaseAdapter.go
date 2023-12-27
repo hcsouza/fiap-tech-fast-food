226886583728099ba28e6139e3132610bed1caba
SalehKhalilMP <saleh.khalil@mercadolivre.com>
2023-12-26 22:41:31 -0300

    feat: update database adapter interface

--- internal/core/repository/databaseAdapter.go
+++ internal/core/repository/databaseAdapter.go
@@ -3,4 +3,6 @@ package repository
 type IDatabaseAdapter interface {
 	FindOne(key, value string) (interface{}, error)
 	Save(identifier string, data interface{}) (id interface{}, err error)
+	Update(identifier string, data interface{}) (id interface{}, err error)
+	Delete(identifier string) (id interface{}, err error)
 }
