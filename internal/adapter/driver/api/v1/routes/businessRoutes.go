cba128d3794feae74461e7a8381f49fa7fded736
SalehKhalilMP <saleh.khalil@mercadolivre.com>
2023-12-26 22:39:05 -0300

    feat: register product handler on business routes

--- internal/adapter/driver/api/v1/routes/businessRoutes.go
+++ internal/adapter/driver/api/v1/routes/businessRoutes.go
@@ -4,11 +4,13 @@ import (
 	"github.com/gin-gonic/gin"
 	adapterDB "github.com/hcsouza/fiap-tech-fast-food/internal/adapter/driven/repository/mongodb"
 	customerDB "github.com/hcsouza/fiap-tech-fast-food/internal/adapter/driven/repository/mongodb/customer"
+	productDB "github.com/hcsouza/fiap-tech-fast-food/internal/adapter/driven/repository/mongodb/product"
 	"github.com/hcsouza/fiap-tech-fast-food/internal/adapter/infra/config"
 
 	"github.com/hcsouza/fiap-tech-fast-food/internal/adapter/driver/api/v1/handlers"
 	"github.com/hcsouza/fiap-tech-fast-food/internal/core/domain"
 	"github.com/hcsouza/fiap-tech-fast-food/internal/core/useCases/customer"
+	"github.com/hcsouza/fiap-tech-fast-food/internal/core/useCases/product"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -28,4 +30,17 @@ func registerCustomerHandler(groupServer *gin.RouterGroup, dbClient mongo.Client
 
 	customerInteractor := customer.NewCustomerUseCase(repo)
 	handlers.NewCustomerHandler(groupServer, customerInteractor)
+}
+
+func registerProductHandler(groupServer *gin.RouterGroup, dbClient mongo.Client) {
+	repo := productDB.NewProductRepository(
+		adapterDB.NewMongoAdapter[domain.Product](
+			dbClient,
+			config.GetMongoCfg().Database,
+			domain.Customer{}.CollectionName(),
+		),
+	)
+
+	productInteractor := product.NewProductUseCase(repo)
+	handlers.NewProductHandler(groupServer, productInteractor)
 }
