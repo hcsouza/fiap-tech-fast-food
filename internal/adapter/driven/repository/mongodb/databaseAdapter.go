bf7c914fb6818ece13d230977910c888cf91ba0f
SalehKhalilMP <saleh.khalil@mercadolivre.com>
2023-12-26 22:40:08 -0300

    feat: add update and remove methods on database adapter

--- internal/adapter/driven/repository/mongodb/databaseAdapter.go
+++ internal/adapter/driven/repository/mongodb/databaseAdapter.go
@@ -52,3 +52,19 @@ func (ad *mongoAdapter[T]) Save(identifier string, data interface{}) (interface{
 	res, err := ad.collection.InsertOne(ctx, data)
 	return res.InsertedID, err
 }
+
+func (ad *mongoAdapter[T]) Update(identifier string, data interface{}) (interface{}, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	res, err := ad.collection.UpdateOne(ctx, bson.M{"_id": identifier}, bson.D{{"$set", data}})
+	return res.UpsertedID, err
+}
+
+func (ad *mongoAdapter[T]) Delete(identifier string) (interface{}, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	res, err := ad.collection.DeleteOne(ctx, bson.M{"_id": identifier})
+	return res.DeletedCount, err
+}
