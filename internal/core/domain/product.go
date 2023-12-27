d2c9e62522f70778921747e76068b76ec84879d8
SalehKhalilMP <saleh.khalil@mercadolivre.com>
2023-12-26 22:45:08 -0300

    feat: add IsValid, ToMongo and CollectionName on Product entity

--- internal/core/domain/product.go
+++ internal/core/domain/product.go
@@ -1,10 +1,32 @@
 package domain
 
-import . "github.com/hcsouza/fiap-tech-fast-food/internal/core/valueObject/category"
+import (
+	"github.com/google/uuid"
+	. "github.com/hcsouza/fiap-tech-fast-food/internal/core/valueObject/category"
+)
 
 type Product struct {
+	ID       string   `json:"id"`
 	Name     string   `json:"name"`
 	Price    float64  `json:"price"`
 	Quantity int      `json:"quantity"`
 	Category Category `json:"category"`
+}
+
+func (p *Product) IsValid() bool {
+	return p.Category.IsValid() // TODO: Melhorar validação
+}
+
+func (p *Product) ToMongo() map[string]interface{} {
+	return map[string]interface{}{
+		"_id":      uuid.New().String(),
+		"name":     p.Name,
+		"price":    p.Price,
+		"quantity": p.Quantity,
+		"category": p.Category,
+	}
+}
+
+func (p *Product) CollectionName() string {
+	return "product"
 }
