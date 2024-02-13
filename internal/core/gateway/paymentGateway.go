bdf8204e1c8b5fb43652e50b2f3075f1899cab91
SalehKhalilMP <saleh.khalil@mercadolivre.com>
2024-02-13 19:25:05 -0300

    refactor: remove previous fake checkout logical

--- internal/core/gateway/paymentGateway.go
+++
@@ -1,9 +0,0 @@
-package gateway
-
-import (
-	. "github.com/hcsouza/fiap-tech-fast-food/internal/core/valueObject/qrCodeResponse"
-)
-
-type PaymentGateway interface {
-	GetQRCodeFromOrder(orderId string, value float64) (QRCodeResponse, error)
-}
