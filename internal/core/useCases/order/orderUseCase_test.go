a8f5f461e30b681e6016410a9e44796b0508dc67
SalehKhalilMP <saleh.khalil@mercadolivre.com>
2024-02-18 19:49:52 -0300

    test: create test suite to method findAll on order use case

--- internal/core/useCases/order/orderUseCase_test.go
+++ internal/core/useCases/order/orderUseCase_test.go
@@ -2,17 +2,20 @@ package order_test
 
 import (
 	"errors"
+	"net/http"
+	"testing"
+	"time"
+
 	gw "github.com/hcsouza/fiap-tech-fast-food/internal/adapter/driven/httpClient"
 	pg "github.com/hcsouza/fiap-tech-fast-food/internal/adapter/driven/httpClient/paymentGateway"
 	"github.com/hcsouza/fiap-tech-fast-food/internal/core/domain"
 	"github.com/hcsouza/fiap-tech-fast-food/internal/core/useCases/customer"
 	"github.com/hcsouza/fiap-tech-fast-food/internal/core/useCases/order"
 	"github.com/hcsouza/fiap-tech-fast-food/internal/core/useCases/product"
+	"github.com/hcsouza/fiap-tech-fast-food/internal/core/valueObject/customTime"
 	. "github.com/hcsouza/fiap-tech-fast-food/internal/core/valueObject/orderStatus"
 	"github.com/hcsouza/fiap-tech-fast-food/test/mocks"
 	"github.com/stretchr/testify/assert"
-	"net/http"
-	"testing"
 )
 
 var orderRepositoryMock *mocks.MockOrderRepository
@@ -109,4 +112,182 @@ func TestOrderUseCase(t *testing.T) {
 		assert.Nil(t, resultOrders)
 	})
 
+	t.Run("should return all orders sorted by READY > PREPARING > RECEIVED", func(t *testing.T) {
+		expectedOrders := []domain.Order{
+			{ID: "1", OrderStatus: ORDER_BEING_PREPARED},
+			{ID: "2", OrderStatus: ORDER_RECEIVED},
+			{ID: "3", OrderStatus: ORDER_READY},
+			{ID: "4", OrderStatus: ORDER_READY},
+			{ID: "5", OrderStatus: ORDER_RECEIVED},
+			{ID: "6", OrderStatus: ORDER_BEING_PREPARED},
+		}
+
+		orderRepositoryMock = mocks.NewMockOrderRepository(t)
+		orderRepositoryMock.On("FindAll").Return(expectedOrders, nil)
+
+		useCase := order.NewOrderUseCase(orderRepositoryMock, productUseCase, customerUseCase, paymentGateway)
+
+		resultOrders, err := useCase.FindAll()
+
+		assert.NoError(t, err)
+		assert.Len(t, resultOrders, len(expectedOrders))
+		assert.Equal(t, ORDER_READY, resultOrders[0].OrderStatus)
+		assert.Equal(t, ORDER_READY, resultOrders[1].OrderStatus)
+		assert.Equal(t, ORDER_BEING_PREPARED, resultOrders[2].OrderStatus)
+		assert.Equal(t, ORDER_BEING_PREPARED, resultOrders[3].OrderStatus)
+		assert.Equal(t, ORDER_RECEIVED, resultOrders[4].OrderStatus)
+		assert.Equal(t, ORDER_RECEIVED, resultOrders[4].OrderStatus)
+	})
+
+	t.Run("should return all orders sorted by createdAt", func(t *testing.T) {
+		currentTime := time.Now()
+
+		expectedOrders := []domain.Order{
+			{ID: "1", OrderStatus: ORDER_READY, CreatedAt: customTime.CustomTime{
+				Time: currentTime.Add(
+					time.Hour*time.Duration(2) +
+						time.Minute*time.Duration(0) +
+						time.Second*time.Duration(0),
+				),
+			}},
+			{ID: "2", OrderStatus: ORDER_READY, CreatedAt: customTime.CustomTime{
+				Time: currentTime.Add(
+					time.Hour*time.Duration(1) +
+						time.Minute*time.Duration(0) +
+						time.Second*time.Duration(0),
+				),
+			}},
+			{ID: "3", OrderStatus: ORDER_BEING_PREPARED, CreatedAt: customTime.CustomTime{
+				Time: currentTime.Add(
+					time.Hour*time.Duration(4) +
+						time.Minute*time.Duration(0) +
+						time.Second*time.Duration(0),
+				),
+			}},
+			{ID: "4", OrderStatus: ORDER_BEING_PREPARED, CreatedAt: customTime.CustomTime{
+				Time: currentTime.Add(
+					time.Hour*time.Duration(3) +
+						time.Minute*time.Duration(0) +
+						time.Second*time.Duration(0),
+				),
+			}},
+			{ID: "5", OrderStatus: ORDER_RECEIVED, CreatedAt: customTime.CustomTime{
+				Time: currentTime.Add(
+					time.Hour*time.Duration(6) +
+						time.Minute*time.Duration(0) +
+						time.Second*time.Duration(0),
+				),
+			}},
+			{ID: "6", OrderStatus: ORDER_RECEIVED, CreatedAt: customTime.CustomTime{
+				Time: currentTime.Add(
+					time.Hour*time.Duration(5) +
+						time.Minute*time.Duration(0) +
+						time.Second*time.Duration(0),
+				),
+			}},
+		}
+
+		orderRepositoryMock = mocks.NewMockOrderRepository(t)
+		orderRepositoryMock.On("FindAll").Return(expectedOrders, nil)
+
+		useCase := order.NewOrderUseCase(orderRepositoryMock, productUseCase, customerUseCase, paymentGateway)
+
+		resultOrders, err := useCase.FindAll()
+
+		assert.NoError(t, err)
+		assert.Len(t, resultOrders, len(expectedOrders))
+		assert.Equal(t, resultOrders[0].OrderStatus, ORDER_READY)
+		assert.Equal(t, resultOrders[1].OrderStatus, ORDER_READY)
+		assert.Equal(t, resultOrders[2].OrderStatus, ORDER_BEING_PREPARED)
+		assert.Equal(t, resultOrders[3].OrderStatus, ORDER_BEING_PREPARED)
+		assert.Equal(t, resultOrders[4].OrderStatus, ORDER_RECEIVED)
+		assert.Equal(t, resultOrders[5].OrderStatus, ORDER_RECEIVED)
+		assert.True(t, resultOrders[0].CreatedAt.Before(resultOrders[1].CreatedAt.Time))
+		assert.True(t, resultOrders[1].CreatedAt.Before(resultOrders[2].CreatedAt.Time))
+		assert.True(t, resultOrders[2].CreatedAt.Before(resultOrders[3].CreatedAt.Time))
+		assert.True(t, resultOrders[3].CreatedAt.Before(resultOrders[4].CreatedAt.Time))
+		assert.True(t, resultOrders[4].CreatedAt.Before(resultOrders[5].CreatedAt.Time))
+	})
+
+	t.Run("should return all orders without COMPLETED status", func(t *testing.T) {
+		currentTime := time.Now()
+
+		expectedOrders := []domain.Order{
+			{ID: "1", OrderStatus: ORDER_READY, CreatedAt: customTime.CustomTime{
+				Time: currentTime.Add(
+					time.Hour*time.Duration(2) +
+						time.Minute*time.Duration(0) +
+						time.Second*time.Duration(0),
+				),
+			}},
+			{ID: "2", OrderStatus: ORDER_READY, CreatedAt: customTime.CustomTime{
+				Time: currentTime.Add(
+					time.Hour*time.Duration(1) +
+						time.Minute*time.Duration(0) +
+						time.Second*time.Duration(0),
+				),
+			}},
+			{ID: "3", OrderStatus: ORDER_BEING_PREPARED, CreatedAt: customTime.CustomTime{
+				Time: currentTime.Add(
+					time.Hour*time.Duration(4) +
+						time.Minute*time.Duration(0) +
+						time.Second*time.Duration(0),
+				),
+			}},
+			{ID: "4", OrderStatus: ORDER_COMPLETED, CreatedAt: customTime.CustomTime{
+				Time: currentTime.Add(
+					time.Hour*time.Duration(4) +
+						time.Minute*time.Duration(0) +
+						time.Second*time.Duration(0),
+				),
+			}},
+			{ID: "5", OrderStatus: ORDER_BEING_PREPARED, CreatedAt: customTime.CustomTime{
+				Time: currentTime.Add(
+					time.Hour*time.Duration(3) +
+						time.Minute*time.Duration(0) +
+						time.Second*time.Duration(0),
+				),
+			}},
+			{ID: "6", OrderStatus: ORDER_RECEIVED, CreatedAt: customTime.CustomTime{
+				Time: currentTime.Add(
+					time.Hour*time.Duration(6) +
+						time.Minute*time.Duration(0) +
+						time.Second*time.Duration(0),
+				),
+			}},
+			{ID: "7", OrderStatus: ORDER_RECEIVED, CreatedAt: customTime.CustomTime{
+				Time: currentTime.Add(
+					time.Hour*time.Duration(5) +
+						time.Minute*time.Duration(0) +
+						time.Second*time.Duration(0),
+				),
+			}},
+		}
+
+		orderRepositoryMock = mocks.NewMockOrderRepository(t)
+		orderRepositoryMock.On("FindAll").Return(expectedOrders, nil)
+
+		useCase := order.NewOrderUseCase(orderRepositoryMock, productUseCase, customerUseCase, paymentGateway)
+
+		resultOrders, err := useCase.FindAll()
+
+		assert.NoError(t, err)
+		assert.Len(t, resultOrders, len(expectedOrders)-1)
+
+		for _, order := range resultOrders {
+			assert.NotEqual(t, ORDER_COMPLETED, order.OrderStatus)
+		}
+
+		assert.Equal(t, resultOrders[0].OrderStatus, ORDER_READY)
+		assert.Equal(t, resultOrders[1].OrderStatus, ORDER_READY)
+		assert.Equal(t, resultOrders[2].OrderStatus, ORDER_BEING_PREPARED)
+		assert.Equal(t, resultOrders[3].OrderStatus, ORDER_BEING_PREPARED)
+		assert.Equal(t, resultOrders[4].OrderStatus, ORDER_RECEIVED)
+		assert.Equal(t, resultOrders[5].OrderStatus, ORDER_RECEIVED)
+		assert.True(t, resultOrders[0].CreatedAt.Before(resultOrders[1].CreatedAt.Time))
+		assert.True(t, resultOrders[1].CreatedAt.Before(resultOrders[2].CreatedAt.Time))
+		assert.True(t, resultOrders[2].CreatedAt.Before(resultOrders[3].CreatedAt.Time))
+		assert.True(t, resultOrders[3].CreatedAt.Before(resultOrders[4].CreatedAt.Time))
+		assert.True(t, resultOrders[4].CreatedAt.Before(resultOrders[5].CreatedAt.Time))
+	})
 }
